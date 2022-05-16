39012c52cd9c7924f17d570402cf507871dce667
trayichaganti <trayivannu@gmail.com>
2022-05-16 19:04:19 +0200

    passed

--- src/main/java/SUM.java
+++ src/main/java/SUM.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class SUM {
+public class SUM {
+    public static void main(String[] args){
+
+    }
+    public int SumOfThree(int a, int b, int c){
+        if( a == b || b == c || a ==c )
+            return 0;
+        else{
+            return a+b+c;
+        }
+
+
+    }
+
+
 }
+
+
+
--- src/test/java/SUMTest.java
+++ src/test/java/SUMTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
-class SUMTest {
-  
+
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class SUMTest {
+    public static SUM obj;
+
+    @BeforeClass
+    public static void setup() {
+        obj = new SUM();
+        assertNotEquals(obj, null);
+    }
+
+    @Test
+    public void SumOfThree1() {
+        assertEquals(9, obj.SumOfThree(3, 4, 2));
+    }
 }
